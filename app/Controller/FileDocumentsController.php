2cfe9b63a7d41b29d143dfa85232928b12af020f
tenengi <tenengi@tenengi>
2015-01-22 14:15:54 +0700

    add FileDocumentsController

--- app/Controller/FileDocumentsController.php
+++ app/Controller/FileDocumentsController.php
@@ -1,109 +1,120 @@
 <?php
+
 App::uses('AppController', 'Controller');
-/**
- * FileDocuments Controller
- *
- * @property FileDocument $FileDocument
- * @property PaginatorComponent $Paginator
- * @property SessionComponent $Session
- */
+
 class FileDocumentsController extends AppController {
 
-/**
- * Components
- *
- * @var array
- */
-	public $components = array('Paginator', 'Session');
+    public $components = array('Paginator', 'Session');
 
-/**
- * index method
- *
- * @return void
- */
-	public function index() {
-		$this->FileDocument->recursive = 0;
-		$this->set('fileDocuments', $this->Paginator->paginate());
-	}
+    public function index() {
+        $this->layout = "carousel";
+        $this->FileDocument->recursive = 0;
+        $this->set('fileDocuments', $this->Paginator->paginate());
+    }
 
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		if (!$this->FileDocument->exists($id)) {
-			throw new NotFoundException(__('Invalid file document'));
-		}
-		$options = array('conditions' => array('FileDocument.' . $this->FileDocument->primaryKey => $id));
-		$this->set('fileDocument', $this->FileDocument->find('first', $options));
-	}
+    public function view($id = null) {
+        if (!$this->FileDocument->exists($id)) {
+            throw new NotFoundException(__('Invalid file document'));
+        }
+        $options = array('conditions' => array('FileDocument.' . $this->FileDocument->primaryKey => $id));
+        $this->set('fileDocument', $this->FileDocument->find('first', $options));
+    }
 
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->FileDocument->create();
-			if ($this->FileDocument->save($this->request->data)) {
-				$this->Session->setFlash(__('The file document has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The file document could not be saved. Please, try again.'));
-			}
-		}
-		$assetInformations = $this->FileDocument->AssetInformation->find('list');
-		$this->set(compact('assetInformations'));
-	}
+    public function add($assetInformationId = null) {
 
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		if (!$this->FileDocument->exists($id)) {
-			throw new NotFoundException(__('Invalid file document'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->FileDocument->save($this->request->data)) {
-				$this->Session->setFlash(__('The file document has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The file document could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('FileDocument.' . $this->FileDocument->primaryKey => $id));
-			$this->request->data = $this->FileDocument->find('first', $options);
-		}
-		$assetInformations = $this->FileDocument->AssetInformation->find('list');
-		$this->set(compact('assetInformations'));
-	}
+        if ($this->request->is('post')) {
+            $document = $this->request->data['FileDocument']['document'];
 
-/**
- * delete method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		$this->FileDocument->id = $id;
-		if (!$this->FileDocument->exists()) {
-			throw new NotFoundException(__('Invalid file document'));
-		}
-		$this->request->allowMethod('post', 'delete');
-		if ($this->FileDocument->delete()) {
-			$this->Session->setFlash(__('The file document has been deleted.'));
-		} else {
-			$this->Session->setFlash(__('The file document could not be deleted. Please, try again.'));
-		}
-		return $this->redirect(array('action' => 'index'));
-	}
+            if ($this->isUploadedFile($document)) {
+
+                $doc = pathinfo($document['name'], PATHINFO_EXTENSION);
+                $document['file_doc_path'] = '/files/filedocument';
+                $document['file_doc_file_type'] = $document['type'];
+                $document['file_doc_name'] = 'filedocumentzzz_' . $assetInformationId . '_' . $this->FileDocument->id . '.' . $doc;
+                $currentUser = $this->Session->read('Auth.User');
+                $document['created_by'] = $currentUser['username'];
+                $this->FileDocument->create();
+                
+                if ($this->FileDocument->save($document)) {           
+                    $document['id'] = $this->FileDocument->id;
+                    $document['file_doc_name'] = 'filedocumentzzz_' . $assetInformationId . '_' . $this->FileDocument->id . '.' . $doc;
+                    $this->saveUploadFile($document, 'files/filedocument', $document['file_doc_name']);
+                    $this->FileDocument->save($document);
+
+
+                    return $this->redirect(array('action' => 'index'));
+                } else {
+                    $this->Session->setFlash(__('The filedoument could not be saved. Please, try again.'));
+                }
+            }
+        }
+
+        $assetInformations = $this->FileDocument->AssetInformation->find('list');
+        $this->set(compact('assetInformations'));
+    }
+
+    public function edit($id = null) {
+        if (!$this->FileDocument->exists($id)) {
+            throw new NotFoundException(__('Invalid file document'));
+        }
+        if ($this->request->is(array('post', 'put'))) {
+            if ($this->FileDocument->save($this->request->data)) {
+                $this->Session->setFlash(__('The file document has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The file document could not be saved. Please, try again.'));
+            }
+        } else {
+            $options = array('conditions' => array('FileDocument.' . $this->FileDocument->primaryKey => $id));
+            $this->request->data = $this->FileDocument->find('first', $options);
+        }
+        $assetInformations = $this->FileDocument->AssetInformation->find('list');
+        $this->set(compact('assetInformations'));
+    }
+
+    public function delete($id = null) {
+        $this->FileDocument->id = $id;
+        if (!$this->FileDocument->exists()) {
+            throw new NotFoundException(__('Invalid file document'));
+        }
+        $this->request->allowMethod('post', 'delete');
+        if ($this->FileDocument->delete()) {
+            $this->Session->setFlash(__('The file document has been deleted.'));
+        } else {
+            $this->Session->setFlash(__('The file document could not be deleted. Please, try again.'));
+        }
+        return $this->redirect(array('action' => 'index'));
+    }
+
+    public function isUploadedFile($field) {
+        $document = array(
+            'filedocument/doc' => '.doc',
+            'filedocument/docx' => '.docx',
+            'application/pdf' => '.pdf',
+            'application/msword' => '.msword',
+            'image/jpeg' => '.jpg',
+        );
+
+        if ((isset($field['error']) && $field['error'] == 0) ||
+                (!empty($field['tmp_name']) && $field['tmp_name'] != 'none')
+        ) {
+            if (array_key_exists($field['type'], $document)) {
+                return is_uploaded_file($field['tmp_name']);
+            }
+        }
+
+        return false;
+    }
+
+    public function saveUploadFile($field, $path, $fileName) {
+
+        if (isset($field['error']) && $field['error'] == 0) {
+            move_uploaded_file($field['tmp_name'], WWW_ROOT . $path . '/' . $fileName);
+
+            return $path . '/' . $fileName;
+        }
+
+        return '';
+    }
+
 }
--- app/View/FileDocuments/add.ctp
+++ app/View/FileDocuments/add.ctp
@@ -1,18 +1,19 @@
 <div class="fileDocuments form">
-<?php echo $this->Form->create('FileDocument'); ?>
+<?php echo $this->Form->create('FileDocument',array('type' => 'post','enctype' => 'multipart/form-data')); ?>
 	<fieldset>
 		<legend><?php echo __('Add File Document'); ?></legend>
 	<?php
-		echo $this->Form->input('asset_information_id');
-		echo $this->Form->input('file_doc_name');
-		echo $this->Form->input('file_doc_path');
-		echo $this->Form->input('file_doc_file_type');
-		echo $this->Form->input('created_by');
+		echo $this->Form->input('asset_information_id',array('readonly'=>'true'));
+		echo $this->Form->input('file_doc_name',array('type' => 'hidden'));
+		echo $this->Form->input('file_doc_path',array('type' => 'hidden'));
+		echo $this->Form->input('file_doc_file_type',array('type' => 'hidden'));
+		echo $this->Form->input('created_by',array('type' => 'hidden'));
 	?>
+                <input type ="file" name ="data[FileDocument][document]" />
 	</fieldset>
 <?php echo $this->Form->end(__('Submit')); ?>
 </div>
-<div class="actions">
+<!--<div class="actions">
 	<h3><?php echo __('Actions'); ?></h3>
 	<ul>
 
@@ -20,4 +21,4 @@
 		<li><?php echo $this->Html->link(__('List Asset Informations'), array('controller' => 'asset_informations', 'action' => 'index')); ?> </li>
 		<li><?php echo $this->Html->link(__('New Asset Information'), array('controller' => 'asset_informations', 'action' => 'add')); ?> </li>
 	</ul>
-</div>
+</div>-->
